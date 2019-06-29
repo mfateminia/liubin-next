a4eb3e1dfb8c80916ee49ca4ad3646379345db33
Ali <mfateminia@gmail.com>
2019-06-29 23:46:59 +0200

    chore: store removed

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,6 +1,5 @@
 import React, { PureComponent, Fragment } from 'react';
 import { Provider } from 'mobx-react';
-import Store from '../../Store/store';
 import Nav from './Nav';
 import Footer from './Footer';
 import Head from 'next/head';
@@ -8,7 +7,7 @@ import Head from 'next/head';
 export default class Layout extends PureComponent {
     render() {
         return (
-            <Provider store={Store}>
+            <Provider>
                 <Fragment>
                     <Head>
                         <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" />
